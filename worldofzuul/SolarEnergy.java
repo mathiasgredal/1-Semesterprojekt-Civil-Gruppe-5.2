489967d42836e05e040a7af4371aa3281a7062f4
DominicNr2 <dongu21@student.sdu.dk>
2021-11-02 14:12:12 +0100

    Update SolarEnergy.java

--- worldofzuul/SolarEnergy.java
+++ worldofzuul/SolarEnergy.java
@@ -1,12 +1,12 @@
 package worldofzuul;
 
 public class SolarEnergy extends EnergySource{
-    private static String solEnergyName = "Sol Energy";
-    private static int solEnergyPrice = 3;
-    private static int solEnergyEmission = 10;
-    private static int solEnergyOutput = 101;
+    private static String solarEnergyName = "Sol Energy";
+    private static int solarEnergyPrice = 3;
+    private static int solarEnergyEmission = 10;
+    private static int solarEnergyOutput = 101;
 
     public SolarEnergy(String energyName, int energyPrice, int energyEmission, int energyOutput) {
-        super(solEnergyName, solEnergyPrice, solEnergyEmission, solEnergyOutput);
+        super(solarEnergyName, solarEnergyPrice, solarEnergyEmission, solarEnergyOutput);
     }
 }
