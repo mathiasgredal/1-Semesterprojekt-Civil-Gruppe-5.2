4fd55d95812a66e817261a1628bd544ebb45d118
frmad <frmad21@student.sdu.dk>
2021-11-02 12:08:19 +0100

    Har slettet kommentaren igen

--- worldofzuul/WindEnergy.java
+++ worldofzuul/WindEnergy.java
@@ -4,6 +4,5 @@ public class WindEnergy extends EnergySource{
     public WindEnergy(String energyName, int energyPrice, int energyEmission, int energyOutput) {
         super(energyName, energyPrice, energyEmission, energyOutput);
     }
-
-    //dette er noget
+    
 }
