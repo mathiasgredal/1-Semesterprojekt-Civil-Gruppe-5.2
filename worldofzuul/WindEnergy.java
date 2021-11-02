6064e29a8353146afa930f865c38f14dad92a576
DominicNr2 <dongu21@student.sdu.dk>
2021-11-02 12:33:57 +0100

    Lavede static variabler i for vedvarende energi kilder
    :+1:

--- worldofzuul/WindEnergy.java
+++ worldofzuul/WindEnergy.java
@@ -2,9 +2,10 @@ package worldofzuul;
 
 public class WindEnergy extends EnergySource{
     private static int windEnergyPrice = 1;
-    
+    private static int windEnergyEmission = 0;
+
     public WindEnergy(String energyName, int energyPrice, int energyEmission, int energyOutput) {
-        super(energyName, windEnergyPrice, energyEmission, energyOutput);
+        super(energyName, windEnergyPrice, windEnergyEmission, energyOutput);
     }
     
 }
