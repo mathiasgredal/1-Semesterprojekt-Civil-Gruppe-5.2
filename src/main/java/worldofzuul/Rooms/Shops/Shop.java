21242a1a7e4775aa77766f4037dc29af28373fc5
Mathias Gredal <mathiasgredal@icloud.com>
2021-11-07 14:35:55 +0100

    Made shopItem private and added some comments

--- src/main/java/worldofzuul/Rooms/Shops/Shop.java
+++ src/main/java/worldofzuul/Rooms/Shops/Shop.java
@@ -7,7 +7,7 @@ import worldofzuul.Rooms.Room;
 import java.util.List;
 
 public abstract class Shop extends Room {
-    protected List<Buyable> shopItems;
+    private List<Buyable> shopItems;
 
     public Shop(String name, String description, List<Buyable> shopItems) {
         super(name, description);
@@ -32,8 +32,11 @@ public abstract class Shop extends Room {
 
     public void printShopDetails(double availableMoney) {
         for (int i = 0; i < shopItems.size(); i++) {
+            // This statement, prints ansi escape codes to color the shop item red or green ¨
+            // depending on whether the player can afford the item
             System.out.print(availableMoney > shopItems.get(i).getPrice() ? "\u001B[32m" : "\u001B[31m");
             System.out.println("[" + (i + 1) + "] " + shopItems.get(i).getInfo());
+            // This escape code resets the color
             System.out.print("\u001B[0m");
         }
     }
