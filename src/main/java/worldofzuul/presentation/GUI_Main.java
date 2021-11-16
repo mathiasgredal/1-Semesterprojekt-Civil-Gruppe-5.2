35b5802eab39044ae738595b510b8b8eeab4b7e0
Nolle10 <oliverksvendsen@gmail.com>
2021-11-16 17:12:48 +0100

    Added some views and some classes

--- src/main/java/worldofzuul/presentation/GUI_Main.java
+++ src/main/java/worldofzuul/presentation/GUI_Main.java
@@ -5,9 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class GUI_Main extends Application {
+
+    public static boolean nextScene = false;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,9 +20,14 @@ public class GUI_Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GUI_Main_Controller.class.getResource("/worldofzuul.presentation/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+
         primaryStage.setTitle("Hello!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
+
+
     }
 }
--- src/main/java/worldofzuul/presentation/GUI_Main_Controller.java
+++ src/main/java/worldofzuul/presentation/GUI_Main_Controller.java
@@ -1,8 +1,32 @@
 package worldofzuul.presentation;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.EventObject;
 
 
 public class GUI_Main_Controller {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    public void nextScene(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/worldofzuul.presentation/asd.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage window = (Stage) root.getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+    }
 }
