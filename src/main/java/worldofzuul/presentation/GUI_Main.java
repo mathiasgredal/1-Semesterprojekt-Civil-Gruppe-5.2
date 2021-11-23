bd45c18896b136ca74906067a09b5a1a56ee2279
frmad <frmad21@student.sdu.dk>
2021-11-23 15:57:32 +0100

    I have inserted pictures and made some changes to the introduction page

--- src/main/java/worldofzuul/presentation/GUI_Main.java
+++ src/main/java/worldofzuul/presentation/GUI_Main.java
@@ -4,19 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-import java.io.FileInputStream;
 
 public class GUI_Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/introduktion.fxml")));
+        Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/introduction.fxml")));
         primaryStage.setTitle("Green House Jazz");
-        primaryStage.setScene(new Scene(root, 750, 500));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
--- src/main/java/worldofzuul/presentation/SceneController.java
+++ src/main/java/worldofzuul/presentation/SceneController.java
@@ -3,11 +3,13 @@ package worldofzuul.presentation;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
@@ -17,66 +19,52 @@ import java.util.ResourceBundle;
 
 public class SceneController {
     @FXML
-    Button btnHouse, btnBuildArea, btnCrossRoad, btnShopArea, btnEnergyShop, btnRetailShop;
+    Button btnHouse, btnBuildArea, btnShopArea, btnEnergyShop, btnRetailShop;
 
-    //methods
-    public void handelBtnHouse() throws Exception{
+    //methods for window change
+    public void handleBtnHouse() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/house.fxml")));
 
         Stage window = (Stage) btnHouse.getScene().getWindow();
-        window.setScene(new Scene(root, 750, 500));
+        window.setScene(new Scene(root, 600, 400));
     }
 
-    public void handelBtnBuildArea() throws Exception{
+    public void handleBtnBuildArea() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/buildArea.fxml")));
 
-        //Creating an image
-        Image image = new Image(new FileInputStream("@/worldofzuul/images/Himmel.jpg"));
-
-        //Setting the image view
-        ImageView imageSky = new ImageView(image);
-
         Stage window = (Stage) btnBuildArea.getScene().getWindow();
-        window.setScene(new Scene(root, 750, 500));
+        window.setScene(new Scene(root, 600, 400));
     }
 
-    public void handelBtnCrossRoad() throws Exception{
+    /*public void handelBtnCrossRoad() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/crossRoad.fxml")));
 
         Stage window = (Stage) btnCrossRoad.getScene().getWindow();
-        window.setScene(new Scene(root, 750, 500));
-    }
+        window.setScene(new Scene(root, 600, 400));
+    }*/
 
-    public void handelBtnShopArea() throws Exception{
+    public void handleBtnShopArea() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/shopArea.fxml")));
 
         Stage window = (Stage) btnShopArea.getScene().getWindow();
-        window.setScene(new Scene(root, 750, 500));
+        window.setScene(new Scene(root, 600, 400));
     }
 
-    public void handelBtnEnergyShop() throws Exception{
+    public void handleBtnEnergyShop() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/energyShop.fxml")));
 
         Stage window = (Stage) btnEnergyShop.getScene().getWindow();
-        window.setScene(new Scene(root, 750, 500));
+        window.setScene(new Scene(root, 600, 400));
     }
 
-    public void handelBtnRetailShop() throws Exception{
+    public void handleBtnRetailShop() throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(("/worldofzuul.presentation/retailShop.fxml")));
 
         Stage window = (Stage) btnRetailShop.getScene().getWindow();
-        window.setScene(new Scene(root, 750, 500));
-    }
-
-    @FXML
-    //det kan godt være den ikke skal være en metode men den virkede ikke på nogen måde alligvel så nu står den bare lige sådan
-    public void handleImageSky() throws FileNotFoundException {
-        //Creating an image
-        Image image = new Image(new FileInputStream("@/worldofzuul/images/Himmel.jpg"));
-
-        //Setting the image view
-        ImageView imageSky = new ImageView(image);
-
+        window.setScene(new Scene(root, 600, 400));
     }
 
 }
+
+
+
