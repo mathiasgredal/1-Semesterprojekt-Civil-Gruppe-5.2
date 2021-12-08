7caa0d62f1dadba74c3a321aa21a64b09d667ce5
DominicNr2 <dongu21@student.sdu.dk>
2021-12-08 12:33:05 +0100

    REworked teh highscore og ændrede lidt i scene controlleren

--- src/main/java/worldofzuul/presentation/RecapController.java
+++ src/main/java/worldofzuul/presentation/RecapController.java
@@ -4,22 +4,25 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import worldofzuul.Game;
 
+import java.text.DecimalFormat;
+
 public class RecapController {
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
     @FXML
     private Label emissionScore, moneyEarnedScore, money, highScore;
 
     @FXML
     public void initialize(){
-        int emission = Game.instance.getPlayer().calculateTotalEmission();
-        int moneyEarned = (int)Game.instance.getSoldEnergyPrice();
-        int excessMoney = (int)Game.instance.getPlayer().getPlayerEconomy();
-        int highscore = (50000/(emission/1000))+moneyEarned+excessMoney;
+        double emission = Game.instance.getPlayer().calculateTotalEmission();
+        double moneyEarned = Game.instance.getSoldEnergyPrice();
+        double excessMoney = Game.instance.getPlayer().getPlayerEconomy();
+        int highscore = (int)((50000/(emission/1000))+moneyEarned+excessMoney);
 
 
-        emissionScore.setText(String.valueOf(emission));
-        moneyEarnedScore.setText(String.valueOf(moneyEarned));
-        money.setText(String.valueOf(excessMoney));
+        emissionScore.setText(decimalFormat.format(emission) + " kg CO\u2082");
+        moneyEarnedScore.setText(decimalFormat.format(moneyEarned) + " DKK");
+        money.setText(decimalFormat.format(excessMoney) + " DKK");
         highScore.setText(String.valueOf(highscore));
 
     }
