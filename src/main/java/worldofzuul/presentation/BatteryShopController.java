4fd0423d3e7ef8ca6aa14d42d1b856c58b7d061c
Nolle10 <oliverksvendsen@gmail.com>
2021-12-10 16:09:45 +0100

    Optimized the code, by deleting some unused imports
    L337 PR0G4MM3R

--- src/main/java/worldofzuul/presentation/BatteryShopController.java
+++ src/main/java/worldofzuul/presentation/BatteryShopController.java
@@ -1,22 +1,5 @@
 package worldofzuul.presentation;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.Node;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import worldofzuul.Game;
-import worldofzuul.Input.Command;
-import worldofzuul.Input.CommandWord;
-import worldofzuul.Items.EnergySource;
-import worldofzuul.Rooms.Shops.EnergyShop;
-import worldofzuul.Rooms.Shops.Shop;
-
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
-
 public class BatteryShopController extends ShopController {
     public void handleBtnHouse() throws Exception {
         GUI_Main.setRoot("house");
--- src/main/java/worldofzuul/presentation/SolarShopController.java
+++ src/main/java/worldofzuul/presentation/SolarShopController.java
@@ -1,36 +1,10 @@
 package worldofzuul.presentation;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-import worldofzuul.Game;
-import worldofzuul.Input.Command;
-import worldofzuul.Input.CommandWord;
-import worldofzuul.Items.EnergySource;
-import worldofzuul.Rooms.Shops.Shop;
-
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 
 public class SolarShopController extends ShopController {
     public void handleBtnHouse(ActionEvent actionEvent) throws IOException {
         GUI_Main.setRoot("house");
-    }
-
-    public String getShopName() {
-        String[] arrOfLocation = location.getFile().split("/");
-        String[] shopName = arrOfLocation[arrOfLocation.length - 1].split("\\.");
-        String finalShopName = URLDecoder.decode(shopName[0], StandardCharsets.UTF_8);
-
-        return finalShopName;
     }
 }
