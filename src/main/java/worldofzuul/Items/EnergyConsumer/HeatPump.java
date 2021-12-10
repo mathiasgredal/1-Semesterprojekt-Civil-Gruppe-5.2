bd46436dd7f8e2d246ad95c0e9f37b171c7312e4
DominicNr2 <dongu21@student.sdu.dk>
2021-12-10 17:29:24 +0100

    Added komments to items mappen

--- src/main/java/worldofzuul/Items/EnergyConsumer/HeatPump.java
+++ src/main/java/worldofzuul/Items/EnergyConsumer/HeatPump.java
@@ -1,6 +1,9 @@
 package worldofzuul.Items.EnergyConsumer;
 
 public class HeatPump extends Heating {
+    /**
+     * The HeatPump class invokes the constructor from the parent class, and fill the parameters
+     */
     public HeatPump() {
         super("Heat pump", 76000, 0, 245, 0);
     }
--- src/main/java/worldofzuul/Items/EnergyConsumer/Heating.java
+++ src/main/java/worldofzuul/Items/EnergyConsumer/Heating.java
@@ -1,6 +1,9 @@
 package worldofzuul.Items.EnergyConsumer;
 
 public abstract class Heating extends EnergyConsumer {
+    /**
+     * Abstract Heating class constructor which inherit its contructor from the EnergyConsumer class
+     */
     public Heating(String name, double price, double yearlyEmission, double yearlyEnergyConsumption, double yearlyCost) {
         super(name, price, yearlyEmission, yearlyEnergyConsumption, yearlyCost);
     }
